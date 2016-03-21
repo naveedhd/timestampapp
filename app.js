6a917eb3091cc546faae7dd0a5125dbf4bc5a419
Naveed Usmani <naveedhd@gmail.com>
2016-03-21 14:41:16 +0900

    Changed url name

--- app.js
+++ app.js
@@ -34,8 +34,7 @@ app.param('timestamp', function(req, res, next, timestamp) {
 
 });
 
-app.get('/', function(req, res) {
-  //res.send('Hello World');
+app.get('/', function(req, res) {  
   res.sendFile(__dirname + '/public/index.html');
 });
 
